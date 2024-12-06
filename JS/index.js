f1093c6e2d5c73d040a737e3279c4a3d25d5b5c0
VijayWhat <vijayrv042@gmail.com>
2024-12-06 20:20:20 +0530

    changes in portfolio

--- JS/index.js
+++ JS/index.js
@@ -1,56 +1,13 @@
 
 const tl = gsap.timeline()
 
-tl.from(".Header .links-box .logo, .Header .links-box .links-box-child .links-item, .Header .social-media-links .social-media-links-box .social-links", {
-    y: -100,
-    duration: 1,
-    delay: .5,
-    opacity: 0,
-    stagger: 0.2
-})
-
-tl.from(".center-box-leftchid-content .bg-color-txt", {
-    x: -100,
-    opacity: 0,
-})
-
-tl.from(".center-box-leftchid-content .main-txt, .center-box-leftchid-content .light-txt-info", {
-    y: 100,
-    opacity: 0,
-    stagger:0.2
-})
-
-tl.from(".btn-box", {
-    x: 100,
-    opacity: 0,
-})
-
-tl.from(".center-box-rightchild .background-cover", {
-      y: 100,
-      opacity: 0,
-})
-
-tl.from(".center-box-rightchild .circle, .center-box-rightchild .circle2,.center-box-rightchild .circle3",{
-    rotate: 360,
-    scale: 0,
-    opacity: 0,
-    stagger: 0.2
-})
-
 tl.from(".center-box-rightchild .home-img", {
     y: 100,
     opacity: 0,
-    // scale: 0,
     opacity: 0
 })
 
-tl.from(".experience-box .number, .experience-box .text-box", {
-    x: -100,
-    opacity: 0,
-    stagger: 0.2
-})
 
-// Scrolltigeer ! important
 
 tl.from(".center-box-right-child .header-txt", {
     x: -100,
@@ -97,7 +54,7 @@ tl.from(".center-box-right-child .brand-info ", {
     stagger: 0.2,
     scrollTrigger: {
         trigger: ".brand-txt",
-        start: "30% 35%",
+        start: "25% 90%",
         end: "35% 20%",
         scrub: 1,
         // markers: true,
@@ -105,19 +62,7 @@ tl.from(".center-box-right-child .brand-info ", {
     }
 })
 
-tl.from(".center-box-left-child  .project-box", {
-    y: 100,
-    opacity:0,
-    stagger: 0.2,
-    scrollTrigger: {
-        trigger: "#works",
-        start: "0% 90%",
-        end: "25% 50%",
-        scrub: 1,
-        // markers: true,
-        
-    }
-})
+
 
 tl.from(".project-heading-txt-box, .project-heading-txt-box i, .project-box .project-info-txt", {
     y: 100,
@@ -125,7 +70,7 @@ tl.from(".project-heading-txt-box, .project-heading-txt-box i, .project-box .pro
     stagger: 0.2,
     scrollTrigger: {
         trigger: "#works",
-        start: "25% 90%",
+        start: "20% 90%",
         end: "50% 50%",
         scrub: 1,
         // markers: true,
